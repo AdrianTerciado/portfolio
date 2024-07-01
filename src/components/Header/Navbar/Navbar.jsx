3c5ea910ad4681c2b639c252ff609e1f1dfdb5ed
AdrianTerciado <terciadojano@gmail.com>
2024-07-01 18:15:15 +0200

    add/hamburguer menu and navbar

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react'
+import Logo from '../Logo/Logo'
+import { Spin as Hamburger } from 'hamburger-react'
+
 const Navbar = () => {
-    return (
-      <nav>
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <nav>
+      <Logo />
+      <div className='burguer-icon'>
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+      </div>
+      <div className={!isOpen ? "menu" : "menu-visible"} >
         <ul>
-            <li>Portfolio</li>
-            <li>About</li>
-            <li>Contact</li>
+          <li>Portfolio</li>
+          <li>About</li>
+          <li>Contact</li>
         </ul>
-      </nav>
-    );
-  };
-  
-  export default Navbar
+      </div>
+    </nav >
+  );
+};
+
+export default Navbar
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,15 @@
 const Main = () => {
-    return (
-      <main>
-        ESTE ES EL Main
-      </main>
-    );
-  };
-  
-  export default Main;
+  return (
+    <main>
+      <section className="portada">
+        <h1>FULL-STACK DEVELOPER & TÉCNICO SUPERIOR EN ELECTRÓNICA</h1>
+        <p>Más de 12 años de experiencia en el sector de la electrónica de consumo.</p>
+        <p>Decidí retomar la parte de programación que estudié en mis estudios de electrónica y dar un cambio en mi carrera.</p>
+        <p>Hice un bootcamp de programación web en TheBridge y me especialicé en el stock MERN/PERN.</p>
+        <p>Lo cual me resultó sencillo gracias a la base que tenía de C++ (Desarrollo de Productos Electrónicos), Java (Desarrollo de aplicaciones Multiplataforma) y Python (autodidacta).</p>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
