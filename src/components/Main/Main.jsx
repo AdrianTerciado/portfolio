ef6ca979566311559d2aca6968c2334e9f976927
Adrián <terciadojano@gmail.com>
2024-07-02 19:04:01 +0200

    added component FollowMouse and transition between menus fixed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,17 @@
+import FollowMouse from './FollowMouse/FollowMouse'
+
 const Main = () => {
   return (
     <main>
       <section className="portada">
         <h1>FULL-STACK DEVELOPER & TÉCNICO SUPERIOR EN ELECTRÓNICA</h1>
         <p>Más de 12 años de experiencia en el sector de la electrónica de consumo.</p>
-        <p>Decidí retomar la parte de programación que estudié en mis estudios de electrónica y dar un cambio en mi carrera.</p>
-        <p>Hice un bootcamp de programación web en TheBridge y me especialicé en el stock MERN/PERN.</p>
-        <p>Lo cual me resultó sencillo gracias a la base que tenía de C++ (Desarrollo de Productos Electrónicos), Java (Desarrollo de aplicaciones Multiplataforma) y Python (autodidacta).</p>
+        <p>Decidí retomar la parte de programación que estudié en mis estudios de electrónica (C++) y dar un cambio en mi carrera.</p>
+        <p>Estudié Java y Python.</p>
+        <p>Finalmente hice un bootcamp de programación web en TheBridge y me especialicé en el stock MERN/PERN.</p>
+      </section>
+      <section>
+            <FollowMouse />
       </section>
     </main>
   );
